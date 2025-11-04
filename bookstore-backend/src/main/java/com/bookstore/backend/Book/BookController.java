df25b2089512a8033b37e17e2847247b51312b7a
Alexis Martinez <alexismartinezsahs17@gmail.com>
2025-11-04 14:40:14 -0800

    CORS fix

--- bookstore-backend/src/main/java/com/bookstore/backend/Book/BookController.java
+++ bookstore-backend/src/main/java/com/bookstore/backend/Book/BookController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:5173")
 public class BookController {
     private final BookRepository bookRepository;
 
